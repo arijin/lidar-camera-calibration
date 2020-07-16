456a936a4caa02ebbedf59e41848df13dd2023bd
arijin <arijin@163.com>
2020-07-16 15:43:11 +0800

    first

---
+++ calibration_camera_lidar/build/atomic_configure/local_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# generated from catkin/cmake/template/local_setup.sh.in
+
+# since this file is sourced either use the provided _CATKIN_SETUP_DIR
+# or fall back to the destination set at configure time
+: ${_CATKIN_SETUP_DIR:=/home/arijin/catkin_robotx/autoware/ros/src/sensing/fusion/packages/calibration_camera_lidar/build/devel}
+CATKIN_SETUP_UTIL_ARGS="--extend --local"
+. "$_CATKIN_SETUP_DIR/setup.sh"
+unset CATKIN_SETUP_UTIL_ARGS
---
+++ calibration_camera_lidar/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/arijin/catkin_robotx/autoware/ros/src/sensing/fusion/packages/calibration_camera_lidar/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/arijin/catkin_robotx/autoware/ros/src/sensing/fusion/packages/calibration_camera_lidar/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/arijin/catkin_robotx/autoware/ros/src/sensing/fusion/packages/calibration_camera_lidar:$ROS_PACKAGE_PATH"
